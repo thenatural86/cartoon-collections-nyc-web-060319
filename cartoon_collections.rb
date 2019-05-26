7410f5c14d397413ca1611301cbb99388b56765f
The Natural <philip.smalls86@gmail.com>
2019-05-26 17:19:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
-  array.map {|word|}.capitalize << "!"
+  array.map do |word|
+    word.capitalize << "!"
 end
 
 def long_planeteer_calls# code an argument here
