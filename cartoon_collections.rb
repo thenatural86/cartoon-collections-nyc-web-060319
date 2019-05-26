0c47c1853515dd277e0b275b1803b6db1aea01a9
The Natural <philip.smalls86@gmail.com>
2019-05-26 17:38:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-    array.include?(cheese)
+    cheese_types.include?(cheese)
   end
 end
 
