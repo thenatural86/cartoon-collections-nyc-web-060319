3abe7673f2d773ca3d851c465b39bb614b1e1121
The Natural <philip.smalls86@gmail.com>
2019-05-26 17:26:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,11 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
+
+
+
