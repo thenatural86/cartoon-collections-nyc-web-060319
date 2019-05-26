136a8eb1ac93f3da9695dbd5a095efa87e1f80b5
The Natural <philip.smalls86@gmail.com>
2019-05-26 17:04:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index { |name, index| }
-  puts "#{(index + 1)}. #{name}"
+  puts "#{index + 1}. #{name}"
 end
 
 def summon_captain_planet# code an argument here
